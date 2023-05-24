41787fd8f71963f82c5a2fb8e1722309cc70bd80
Lukáš Vít <vitl03@vse.cz>
2023-05-24 22:58:14 +0200

    add favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" href="https://www.magexo.cz/coGhk58axEfl4/themes/magexo/favicons/favicon-32x32.png" />
 	<title>Magexo project</title>
 
 	@vite('resources/css/app.css')
